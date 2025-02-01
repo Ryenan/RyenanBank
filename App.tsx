cfbc9a1c35f9ea22738bba188eeb115fefb71eea
Ryenan <ryenanaragaorodrigues@gmail.com>
2025-01-31 21:51:48 -0300

    aa

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Home from "./src/pages/home/home";
 
 export default function App() {
   return <Home />;
+  // StatusBar (tem que fazer ela hidden)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
